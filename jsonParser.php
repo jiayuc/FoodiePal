64947b206b0442d5ca11c1392c50c423ddf3c151
Jiayu Chen <jiayuc@hotmail.com>
2016-03-06 12:32:21 -0600

    init

---
+++ jsonParser.php
@@ -0,0 +1,10 @@
+<?php
+	echo "welcome to parser <br>";
+//=============parse JSON=============
+/*
+$json_string='{"id":1,"name":"foo","email":"foo@foobar.com","interest":["wordpress","php"]} ';  
+$obj=json_decode($json_string);  
+echo $obj->name; //prints foo  
+echo $obj->interest[1]; //prints php  
+*/
+?>
