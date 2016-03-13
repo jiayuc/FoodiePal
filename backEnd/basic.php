8aac4ee95fc82de924b6aeb8725eface9c163b39
Jiayu Chen <jiayuc@hotmail.com>
2016-03-12 18:49:59 -0600

    Update basic.php

--- backEnd/basic.php
+++ backEnd/basic.php
@@ -1,15 +1,12 @@
 <?php
-// copy file content into a string var
 $json_file = file_get_contents('miniB.json');
-// $json_file = '{"business_id": "rncjoVoEFUJGCUoC1JgnUA", "full_address": "8466 W Peoria Ave\nSte 6\nPeoria, AZ 85345", "open": true}';
 // convert the string to a json object
 $jfo = json_decode($json_file);
-// read the title value
 $business_id = $jfo->business_id;
 echo $jfo->full_address ;
-// copy the posts array to a php var
+// copy the array to a php var
 $tags = $jfo->categories;
-// listing posts
+// iterate through
 foreach ($tags as $tag) {
     echo $tag;
 }
